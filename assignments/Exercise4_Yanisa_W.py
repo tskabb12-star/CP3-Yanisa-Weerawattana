2d9143572638e3ade631375816ede52a92c0303e
tskabb12-star <tskabb12@gmail.com>
2026-07-16 11:19:56 +0700

    Delete .gitattributes

---
+++ assignments/Exercise4_Yanisa_W.py
@@ -0,0 +1,10 @@
+foundation_english = 70
+general_business = 80
+introduction_to_computer_systems = 90
+computer_programming = 99.5
+
+print("--- Your Score ---")
+print("Foundation English : ", str(foundation_english))
+print("General Business : ", str(general_business))
+print("Introduction to Computer Systems : ", str(introduction_to_computer_systems))
+print("Computer Programming : ", str(computer_programming))
