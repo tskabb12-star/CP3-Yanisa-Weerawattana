38312701a2b132dd9749061e89d37067a30ef3d8
tskabb12-star <tskabb12@gmail.com>
2026-07-17 12:57:28 +0700

    excercise5

---
+++ assignments/Exercise5_1_Yanisa_W.py
@@ -0,0 +1,18 @@
+print("Exercise 5 : โปรแกรมคำนวณเลข")
+
+a = int(input("ตัวเลขที่ 1 :"))
+b = int(input("ตัวเลขที่ 2 :"))
+
+result_add = "+"
+result_sub = "-"
+result_mul = "*"
+result_div = "/"
+
+print()
+print("นำมาดำเนินการ +,-,*,/ จะได้ :")
+print(a,result_add,b,"=",a+b)
+print(a,result_sub,b,"=",a-b)
+print(a,result_mul,b,"=",a*b)
+print(a,result_div,b,"=",a/b)
+
+
