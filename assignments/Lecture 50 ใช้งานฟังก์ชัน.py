b6e855d2c1340804bb8d73a097f4d576071b8f33
tskabb12-star <tskabb12@gmail.com>
2026-08-02 04:54:12 +0700

    Create Lecture 50 ใช้งานฟังก์ชัน.py

---
+++ assignments/Lecture 50 ใช้งานฟังก์ชัน.py
@@ -0,0 +1,16 @@
+def plusNumber (x,y):
+    print(x+y)
+
+def minusNumber (x,y):
+    print(x-y)
+
+def multiplyNumber (x,y):
+    print(x*y)
+
+def divideNumber (x,y):
+    print(x/y)
+
+plusNumber(10,20)
+minusNumber(10,20)
+multiplyNumber(10,20)
+divideNumber(10,20)
